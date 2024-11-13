d8041b38938d1d94b5add6325303b2747546d523
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2024-11-13 19:51:03 -0300

    idem

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware({
-    publicRoutes: ["/"],
+  publicRoutes: ["/", "/sign-in", "/sign-up", "/api/public(.*)"]
    });
 
 export const config = {
