db8ff55fb61bf402f9fe5a695efbed9c1f4817c5
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2024-11-13 20:06:13 -0300

    asd2
    asd2

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { clerkMiddleware, createRouteMatcher  } from "@clerk/nextjs/server";
 const isProtectedRoute = createRouteMatcher([ '/tools(.*)'])
 export default clerkMiddleware(  (auth, req) =>  {
   if (isProtectedRoute(req)) {
-     auth.protect();
+     auth().protect();
   }
    });
 
