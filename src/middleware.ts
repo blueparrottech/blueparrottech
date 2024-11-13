75a6ae19b3bc34579202eeb7f94be9de6ad91d9f
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2024-11-13 19:57:44 -0300

    asd
    asd

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,10 @@ import { clerkMiddleware, createRouteMatcher  } from "@clerk/nextjs/server";
 
 
 const isProtectedRoute = createRouteMatcher([ '/tools(.*)'])
-export default clerkMiddleware({
-
+export default clerkMiddleware( async (auth, req) =>  {
+  if (isProtectedRoute(req)) {
+    await auth.protect();
+  }
    });
 
 export const config = {
