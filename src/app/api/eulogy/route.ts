21bc0639d0d71db10adbef894acefbf321896788
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2024-11-16 22:39:21 -0300

    idem

--- src/app/api/eulogy/route.ts
+++ src/app/api/eulogy/route.ts
@@ -1,7 +1,10 @@
 // pages/api/eulogy.ts
 import { NextResponse } from 'next/server';
 import { neon } from '@neondatabase/serverless';
-import   FormData   from './tools/eulogygenerator/types'
+import   FormData   from '../../eulogygenerator/types'
+
+
+
 export const config = {
   runtime: 'edge',
 };
