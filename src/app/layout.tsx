1d3770725b03374d5111b12ac36599aa18d3ecab
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2024-11-20 18:10:15 -0300

    add g tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import {
   SignedOut,
   UserButton
 } from '@clerk/nextjs'
-
+import Script from "next/script";
 
 
 
@@ -48,10 +48,48 @@ export default function RootLayout({
 }>) {
   return (
     
+
+
+
+
+
+
+
+
+
+
+
+
+
     <ClerkProvider 
     dynamic
     afterSignOutUrl="./">
     <html lang="en">
+
+
+
+
+
+    <head>
+                <Script
+                  async
+                  src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+                />
+
+                <Script id="google-analytics">
+                  {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}',{
+                    page_path: window.location.pathname,                    
+          });
+                  `}
+                </Script>   
+
+          </head>
+
+
 
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
