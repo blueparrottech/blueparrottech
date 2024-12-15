71fb041195c2451f6c18530553f4051fe32c8093
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2024-12-14 21:42:36 -0300

    asd
    asd

--- src/app/providers.ts
+++ src/app/providers.ts
@@ -1,14 +1,21 @@
-// app/providers.js
-'use client'
-import posthog from 'posthog-js'
-import { PostHogProvider } from 'posthog-js/react'
+// app/providers.tsx
+'use client';
 
-if (typeof window !== 'undefined') {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
-  })
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
+
+interface CSPostHogProviderProps {
+  children: React.ReactNode;
 }
-export function CSPostHogProvider({ children }) {
-    return <PostHogProvider client={posthog}>{children}</PostHogProvider>
+
+// Initialize PostHog only on the client side
+if (typeof window !== 'undefined') {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY ?? '', {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    person_profiles: 'identified_only', // or 'always' for anonymous user profiles
+  });
+}
+
+export function CSPostHogProvider({ children }: CSPostHogProviderProps) {
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 }
