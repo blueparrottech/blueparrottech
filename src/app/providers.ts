a562fc5ef8b31866de0f8f6b13a3d228e64a90ba
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2024-12-14 22:50:26 -0300

    asd
    asd

--- src/app/providers.ts
+++ src/app/providers.ts
@@ -1,22 +1,14 @@
-// app/providers.tsx
+// app/providers.js
 'use client'
-
 import posthog from 'posthog-js'
-import { PostHogProvider as PHProvider } from 'posthog-js/react'
-import { useEffect } from 'react'
+import { PostHogProvider } from 'posthog-js/react'
 
-export function PostHogProvider({ children }: { children: React.ReactNode }) {
-    useEffect(() => {
-      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY ?? "", {
-        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-        person_profiles: 'identified_only',
-        capture_pageview: false // Disable automatic pageview capture, as we capture manually
-      })
-  }, [])
-
-  return (
-    <PHProvider client={posthog}>
-      {children}
-    </PHProvider>
-  )
+if (typeof window !== 'undefined') {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY ?? '', {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+  })
+}
+export function CSPostHogProvider({ children }) {
+    return <PostHogProvider client={posthog}>{children}</PostHogProvider>
 }
