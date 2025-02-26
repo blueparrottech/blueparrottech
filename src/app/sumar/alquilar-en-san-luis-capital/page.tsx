c46721ace90f78b103d701f74534620a656e0d9a
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2025-02-26 14:33:19 -0300

    add sumar apge

--- src/app/sumar/alquilar-en-san-luis-capital/page.tsx
+++ src/app/sumar/alquilar-en-san-luis-capital/page.tsx
@@ -24,7 +24,7 @@ export default function BlogPost() {
 
     <div className="min-h-screen bg-gradient-to-b from-lime-50 to-red-100 text-gray-800 font-serif  rounded-lg shadow-xl ">
 
-<title>SUMar Inmobiliaria — Desarrollo Inmobiliario en San Luis - LimeParrot Tech.Site  </title>
+<title>Alquileres en San Luis - SUMar Inmobiliaria - LimeParrot Tech.Site  </title>
 <meta
   name="description"
   content="Introducing the Luggage Measurement Telegram Bot."
@@ -33,7 +33,8 @@ export default function BlogPost() {
 
       <header className="bg-gray-700 shadow-md ">
         <div className="container mx-auto px-6 py-4 flex justify-between items-center">
-          <h1 className="text-3xl font-bold text-gray-200"> SUMar Inmobiliaria</h1>
+          <h1 className="text-3xl font-bold text-gray-200"> <title>Alquileres en San Luis  </title>
+</h1>
           <Link href="/" className="border-l-4 pl-4 border-blue-500 hover:underline font-mono text-gray-200 " prefetch={false}>
               Go to Homepage
             </Link>
@@ -47,23 +48,23 @@ export default function BlogPost() {
 
       <article className="container rounded-lg shadow-xl px-6 py-12 max-w-4xl prose lg:prose-xl mx-auto p-6">
 
-  <h1 className="text-4xl font-bold mb-6">SUMar Inmobiliaria — Desarrollo Inmobiliario en San Luis, Argentina</h1>
+ 
     
-  <h2 className="text-2xl font-semibold mb-4">SUMar Inmobiliaria tiene más de 10 años de experiencia en el mercado inmobiliario de San Luis.
+  <h2 className="text-2xl font-semibold mb-4">SUMar se especializa alquileres de calidad en varias zonas de la ciudad.  
   </h2>
 
   <Image 
-    src="/luggage-size-checker/1.JPG" 
+    src="/sumar/slider-1.webp" 
     alt="Illustration of measuring luggage with a smartphone" 
     width={800} 
     height={400} 
     className="w-full h-90 object-cover rounded-lg shadow-xl mt-8"
   />
 
-  <h2 className="text-2xl font-semibold mt-12">Ofrece Venta y Alquiler de propiedades como casas, lotes, locales comerciales y oficinas.
+  <h2 className="text-2xl font-semibold my-12">De lo más solicitado son inmuebles para alquilar dentro de la ciudad de San Luis. Departamentos, casas, edificios son ofertas disponibles.
 
 </h2>
-  <p>Podés conocer más de nuestra propuesta en nuestra página web:</p>
+  <p>Podés conocer más de nuestra propuesta en alquileres en nuestra página web:</p>
  
   <Link
     href="https://www.sumarsanluis.com.ar/"
@@ -91,7 +92,7 @@ export default function BlogPost() {
 
       <footer className="bg-gray-700 text-white mt-12 py-8">
         <div className="container mx-auto px-6 text-center">
-          <p>&copy; 2025 SUMar Inmobiliaria — Desarrollo Inmobiliario en San Luis Blog by LimeParrot Tech. Crafting knowledge with compassion and technology.</p>
+          <p>&copy; Blog by LimeParrot Tech. Crafting knowledge with compassion and technology.</p>
         </div>
       </footer>
     </div>
